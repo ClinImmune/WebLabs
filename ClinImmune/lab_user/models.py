78869e56e59b8661629b4a144cf4c01ddf7b2848
Lucas Simon <lucas.simon994@gmail.com>
2013-05-16 11:14:29 -0600

    Initial Commit

---
+++ ClinImmune/lab_user/models.py
@@ -0,0 +1,135 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, 
+    BaseUserManager
+)
+
+class LabUserManager(BaseUserManager):
+    """
+    A new user manager for both standard and administrative users
+    """
+    
+    def create_user(
+            self, 
+            email,
+            first_name,
+            last_name,
+            university=None,
+            job_title=None,
+            bio=None,
+            password=None
+    ):
+        """
+        Creates a standard user with no administrative privledges
+        """
+        if not email:
+            raise ValueError('Users must provide an email')
+            
+        if not first_name:
+            raise ValueError('Users must provide a first name')
+        
+        if not last_name:
+            raise ValueError('Users must provide a last name')
+            
+        if not university:
+            raise ValueError('Users must provide an email address')
+            
+        # Note: a biography and job_title are not required
+        user = self.model(
+            email=LabUserManager.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            university=university,
+            job_title=job_title,
+            bio=bio,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
+        
+    def create_superuser(
+            self, 
+            email,
+            first_name,
+            last_name,
+            university,
+            job_title=None,
+            bio=None,
+            password=None
+    ):
+        """
+        Creates an administrative user
+        """    
+                        
+        user = self.create_user(
+            self, 
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            university=university,
+            job_title=job_title,
+            bio=bio,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user                
+
+class LabUser(AbstractBaseUser):
+    """
+    Model for every user on the site
+    The only required fields are:
+        email,
+        first_name,
+        last_name,
+        university,
+    although, this will be discussed later
+    """
+    email = models.EmailField(
+        verbose_name = 'email address',
+        max_length   = 255,
+        unique       = True,
+        db_index     = True,
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    name_is_public = models.BooleanField(default=True)
+    university = models.CharField(max_length=150)
+    job_title = models.CharField(max_length = 50)
+    bio = models.TextField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    objects = LabUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'first_name',
+        'last_name',
+        'university',
+    ]
+    
+    def get_full_name(self):
+        return self.full_name
+        
+    def get_short_name(self):
+        return self.first_name
+        
+    def __unicode__(self):
+        return self.full_name
+    
+    @property
+    def full_name(self):
+        return self.first_name + " " + self.last_name
+    
+    @property 
+    def is_staff(self):
+        return self.is_admin
+    """
+    @property
+    def jobs(self):
+        return self.jobs_set.all()
+    """
+
---
+++ ClinImmune/lab_user/views.py
@@ -0,0 +1,50 @@
+from .models import LabUser
+from .serializers import UserSerializer
+from rest_framework import generics
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+import json
+
+class NewUser(APIView):
+    """
+    Creates a new user, only accepts POST requests
+    """
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+class UserDetail(APIView):
+    """
+    Allows access to a users data for a profile
+    """
+    def get_object(self, pk):
+        try:
+             return LabUser.objects.get(pk=pk)
+        except LabUser.DoesNotExist:
+            raise Http404
+            
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+        
+    def patch(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)  
