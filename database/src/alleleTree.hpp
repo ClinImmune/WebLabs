9c975cb3ba30d8bad857b7225ac6c3ecc11f3802
Lucas Simon <lucas.simon994@gmail.com>
2013-05-22 09:08:44 -0600

    morning commit

--- database/src/alleleTree.hpp
+++ database/src/alleleTree.hpp
@@ -1,26 +1,25 @@
-// W00t! I win!!!
-
 #ifndef ALLELE_TREE_HEADER
 #define ALLELE_TREE_HEADER
 
 #include <string>
 #include <vector>
 #include <boost/smart_ptr.hpp>
-#include <boost/multi_array.hpp>
 
-typedef std::string str;
+using std::string;
+using std::vector;
+using boost::shared_ptr;
 
 // Defines structure for a generic allele
 struct allele {
-	str loci;
-	std::vector<int> position;
+	string loci;
+	vector<int> position;
 };
 
 // Defines a structure to hold a matrix interpretation of a matrix which has 22
 // rows that represent each amino acid type
 // from 0-21
 // ACDEFGHIKLMNPQRSTVWYX
-struct aminoAcidRow {
+struct aminoAcidColumn {
     unsigned short acid[21];
 };
 
@@ -28,7 +27,7 @@ struct aminoAcidRow {
 // inefficient; but, is much quicker for processing tasks
 struct sequenceMatrix {
     int sequenceLength;
-    std::vector<aminoAcidColumn> column;
+    vector<aminoAcidColumn> column;
 };
 
 // Defines structure for an allele node in the allele tree which is a generic
@@ -40,38 +39,38 @@ struct alleleNode {
 	int offset;
 
     allele alleleName;
-	str hla_id;
-	str sequence;
+	string hla_id;
+	string sequence;
 
-	std::vector<boost::shared_ptr<alleleNode> > children;
+	vector<shared_ptr<alleleNode> > children;
 };
 
 // Defines a shared pointer to an alleleNode
-typedef boost::shared_ptr<alleleNode> alleleNodePtr;
+typedef shared_ptr<alleleNode> alleleNodePtr;
 
 // Defines a struct for all of the loci to be used in the lociParentList
 struct locus {
-    str loci;
-    std::vector<alleleNodePtr> children;
+    string loci;
+    vector<alleleNodePtr> children;
 };
 
 // Defines a shared pointer to a locus
-typedef boost::shared_ptr<locus> lociPtr;
+typedef shared_ptr<locus> lociPtr;
 
 // Defines a standard class to handle a tree of loci
 class AlleleTree {
 private:
     // Holds a vector of loci pointers which point to the lowest resolution
     // children
-    std::vector<lociPrt> alleleParentList;
+    vector<lociPtr> alleleParentList;
     // Points to the current node
     alleleNodePtr currentNode;
     // Checks to see if a loci exists in the alleleParentList
-    bool isNewLoci(str);
+    bool isNewLoci(string);
     // returns the name of a loci
-    str getLoci(str);
+    string getLoci(string);
     // returns the matrix form of a sequence
-    sequenceMatrix generateMatrix(str);
+    sequenceMatrix generateMatrix(string);
 public:
     // Constructor and destructor
     AlleleTree();
@@ -80,7 +79,52 @@ public:
     // Returns a pointer to an allele to access information from it
     alleleNodePtr getAllele(allele);
     // Inserts a new allele from data in parameters, will be referenced
-    void insertAllele(str this_sequence, allele this_allele)
+    void insertAllele(alleleNode&);
+    void updateAlleleOffset(allele, string);
+    void updateAllele(alleleNode&);
 };
 
+///////////////////////////////////////////////////////////////////////////////
+// Private methods
+///////////////////////////////////////////////////////////////////////////////
+bool AlleleTree::isNewLoci(string this_loci) {
+    for(int i = 0; i < alleleParentList.size(); i++) {
+        if(this_loci.compare(alleleParentList.at(i)->loci) == 0)
+            return true;
+    }
+    return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Public methods
+///////////////////////////////////////////////////////////////////////////////
+
+AlleleTree::AlleleTree() {/*Creates empty object*/}
+
+AlleleTree::~AlleleTree() {
+    alleleParentList.clear();
+}
+
+void AlleleTree::insertAllele(alleleNode &this_allele) {
+    // Check is the loci is in the parent list
+    bool new_loci = true;
+    for(int i = 0; i < alleleParentList.size(); i++) {
+        if(this_allele
+                .alleleName
+                .loci
+                .compare(alleleParentList
+                .at(i)->loci) == 0
+        ){
+            new_loci = false;
+        }
+    }
+    
+    // If the loci is new, create a new allele parent, append the proper number
+    // of children, and point currentNode to the last child
+    if (new_loci) {
+        alleleNodePtr blah(new alleleNode)
+    }
+    // Iterate through tree until the allele is inserted
+}
+void AlleleTree::updateAllele(alleleNode &this_allele) {}
 #endif
