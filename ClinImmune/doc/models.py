e7f425cae24c0e5b0508218e84fa79fa1dd15ab8
Lucas Simon <lucas.simon994@gmail.com>
2013-06-19 14:49:07 -0600

    Added documentation API, still need to test

--- ClinImmune/doc/models.py
+++ ClinImmune/doc/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.utils.text import slugify
 
+import datetime
+
 """
 The API will expose the models in the following manner
 /docs - GET  => gives list of all documents
         POST => creates a new document, restricted to administrator account
-/docs/verbose_name - GET  => gives a list of chapters
-					 POST => creates a new chapter
-/docs/verbose_name/chap_name - GET  => Gives user a chapter with serialized
-                                       sections
-                             - POST => Creates a new section
-/docs/verbose_name/chap_name#section no API here
+/docs/verbose_name - GET   => gives a list of chapters
+					 POST  => creates a new chapter
+					 DELTE => Deletes a document
+/docs/verbose_name/1 - GET    => Gives user a chapter with serialized
+                               sections
+                       POST   => Creates a new section
+                       DELETE => Deletes a chapter
+/docs/verbose_name/3/section - DELETE => Deletes a section
+						     - PATCH  => Updates a section
+						     - GET    => Returns a section, not public
 """
 
 class Document(models.Model):
@@ -19,12 +25,16 @@ class Document(models.Model):
 	"""
 	title = models.CharField(max_length=144)
 	slug_title = models.SlugField(max_length=30)
-	date_added = models.DateTimeField(auto_now_add=True)
+	date_added = models.DateTimeField(editable=False)
+	last_updated = models.DateTimeField()
 	
 	def __unicode__(self):
 		return self.slug_title
 	
 	def save(self, *args, **kwargs):
+		if not self.id:
+			self.date_added = datetime.datetime.now()
+		self.last_updated = datetime.datetime.now()
 		self.slug_title = slugify(self.title)
 		super(test, self).save(*args, **kwargs)
 	
@@ -35,7 +45,7 @@ class Chapter(models.Model):
 	"""
 	Defines a chapter model which points to a document
 	"""
-	document = models.ForeignKey('Document')
+	document = models.ForeignKey('Document', related_name="chapters")
 	title = models.CharField(max_length=144)
 	slug_title = models.SlugField(max_length=30)
 	number = models.IntegerField(
@@ -48,7 +58,7 @@ class Chapter(models.Model):
 		super(test, self).save(*args, **kwargs)
 	
 	def __unicode__(self):
-		return self.slug_title
+		return '%d: %s' % (self.number, self.title)
 	
 	class Meta:
 		ordering = ['document','number']
--- ClinImmune/doc/views.py
+++ ClinImmune/doc/views.py
@@ -1,1 +1,72 @@
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import *
+from .serializers import *
+
+class DocumentList(APIView):
+	"""
+	Returns a list of documents, up to ten at a time
+	"""
+	paginate_by = 10
+	
+	def get(self, request, format=None):
+		documents = Document.objects.all()
+		serializer = DocumentListSerializer(documents, many=True)
+		return Response(serializer.data)
+		
+	def post(self, request, format=None):
+		serializer = DocumentSerializer(data=request.DATA)
+		if serializer.is_valid()
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Reponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+		
+class DocumentDetail(APIView):
+	def get_object(self, pk):
+		try:
+			return Document.objects.get(pk=pk)
+		except Document.DoesNotExist:
+			raise Http404
+	
+	def get(self, request, pk, format=None):
+		document = self.get_object(pk)
+		serializer = DocumentSerializer(document, data=request.DATA)
+		return Response(serializer.data)
+	
+	"""
+	def patch(self, request, pk, format=None):
+		document = self.get_object(pk)
+		serializer = DocumentSerializer(document, data=request.DATA)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	
+	
+	def delete(self, request, pk, format=None):
+		document = self.get_object(pk)
+		document.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+	"""
+		
+class ChapterDetail(APIView):
+	"""
+	Defines an api endpoint for returning a chapter along with its sections
+	It will return a JSON document
+	"""
+	def get_object(self, pk):
+		try:
+			return Chapter.objects.get(pk=pk)
+		except Document.DoesNotExist:
+			raise Http404
+	
+	def get():
+		chapter = get_object(self, pk):
+		serializer = HyperLinkedChapterSerializer()
+	def post():
+	
+	def delete():
+	
+	
